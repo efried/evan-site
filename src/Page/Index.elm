482fc17ec19e9f78cd511a62ad59019fabd04ad4
Evan Friedenberg <evan.friedenberg@gmail.com>
2022-08-28 14:55:56 -0400

    Add contact links

--- src/Page/Index.elm
+++ src/Page/Index.elm
@@ -1,13 +1,15 @@
 module Page.Index exposing (Data, Model, Msg, page)
 
 import DataSource exposing (DataSource)
-import Element exposing (Element)
+import Element
+import Element.Font as Font
 import Head
 import Head.Seo as Seo
 import Page exposing (Page, StaticPayload)
 import Pages.PageUrl exposing (PageUrl)
 import Pages.Url
 import Shared
+import Url as ElmUrl
 import View exposing (View)
 
 
@@ -61,6 +63,45 @@ type alias Data =
     ()
 
 
+type alias ExternalContact =
+    { source : String
+    , username : String
+    , url : ElmUrl.Url
+    }
+
+
+toExternalContact : ( String, String, String ) -> Maybe ExternalContact
+toExternalContact ( source, username, maybeUrl ) =
+    Maybe.map
+        (\url -> ExternalContact source username url)
+        (ElmUrl.fromString maybeUrl)
+
+
+externalContactInfo : List ( String, String, String )
+externalContactInfo =
+    [ ( "Github", "@efried", "https://github.com/efried" )
+    , ( "LinkedIn", "evanfriedenberg", "https://www.linkedin.com/in/evanfriedenberg" )
+    , ( "Medium", "@evan.friedenberg", "https://www.medium.com/@evan.friedenberg" )
+    , ( "Goodreads", "evanfriedenberg", "https://www.goodreads.com/evanfriedenberg" )
+    ]
+
+
+viewExternalAccount : ExternalContact -> Element.Element msg
+viewExternalAccount { source, username, url } =
+    Element.row
+        []
+        [ Element.column
+            [ Element.width (Element.fillPortion 1) ]
+            [ Element.text (source ++ ": ") ]
+        , Element.column
+            [ Element.width (Element.fillPortion 1), Font.color (Element.rgb255 18 147 216) ]
+            [ Element.newTabLink
+                []
+                { url = ElmUrl.toString url, label = Element.text username }
+            ]
+        ]
+
+
 view :
     Maybe PageUrl
     -> Shared.Model
@@ -73,5 +114,19 @@ view maybeUrl sharedModel static =
             [ Element.width Element.fill
             , Element.height Element.fill
             ]
-            [ Element.column [ Element.centerX, Element.paddingXY 0 100, Element.spacing 16 ] [ Element.text "It's me, Evan Friedenberg", Element.image [ Element.centerX ] { src = "images/avatar.png", description = "Picture of Evan" } ] ]
+            [ Element.column
+                [ Element.width (Element.fillPortion 4), Element.paddingXY 0 100, Element.spacing 16, Element.centerX ]
+                [ Element.el [ Element.centerX ] (Element.text "It's me, Evan Friedenberg")
+                , Element.image [ Element.centerX ] { src = "images/avatar.png", description = "Picture of Evan" }
+                ]
+            , Element.column
+                [ Element.width (Element.fillPortion 2), Element.paddingXY 0 100, Element.spacing 16 ]
+                (Element.text "You can find me on..."
+                    :: (List.filterMap
+                            toExternalContact
+                            externalContactInfo
+                            |> List.map viewExternalAccount
+                       )
+                )
+            ]
     }
